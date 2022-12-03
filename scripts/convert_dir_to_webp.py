0b66a3cbb8e1db8b5cd03b07af485f51ed8399d5
WRadoslaw <r.wyszynski00@gmail.com>
2022-12-03 09:49:43 +0100

    Changes

--- scripts/convert_dir_to_webp.py
+++ scripts/convert_dir_to_webp.py
@@ -5,8 +5,17 @@ import subprocess
 if (len(sys.argv) != 3):
     raise Exception('Wrong number of args')
 
+try:
+    subprocess.run(['cwebp', '--help'])
+except:
+    sys.stderr.write('Error: install cwebp in order to be able to convert files')
+    sys.exit()
+
+
 input_dir = sys.argv[1]
 output_dir = sys.argv[2]
+
+os.makedirs(output_dir, exist_ok=False)
 
 files = os.listdir(input_dir)
 input_paths = [os.path.join(input_dir, file) for file in files]
